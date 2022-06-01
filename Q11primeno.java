267be77873663298163c409d02d36e7d31026f89
pankajpatwa42 <pankajpatwa42@gmail.com>
2022-06-01 19:28:32 +0530

    [Add] Perfect Number Func

--- Q11primeno.java
+++ Q11primeno.java
@@ -4,15 +4,15 @@ public class Q11primeno {
 
 	public static void main(String[] args) {
 		int num = 29;
-	    boolean flag = false;
+	    boolean flag = true;
 	    for (int i = 2; i <= num / 2; ++i) {
-	      if (num % i == 0) {	      // condition for nonprime number
-	        flag = true;
+	      if (num % i == 0) {	      // condition for non prime number
+	        flag = false;
 	        break;
 	      }
 	    }
 
-	    if (!flag)
+	    if (flag)
 	      System.out.println(num + " is a prime number.");
 	    else
 	      System.out.println(num + " is not a prime number.");
